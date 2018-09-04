b0500e8c0e8775e0068f712aa03edf1bc66a166b
GabyFutemma <gaby.futemma@gmail.com>
2018-09-04 19:40:09 -0300

    using eslint in files

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 module.exports.cardValidator = function cardValidator(cardnumber) {
   if (cardnumber === undefined || cardnumber === "") {
     throw new RangeError("You must enter a parameter!");
-  }
-  if (typeof cardnumber !== "number") {
+
+  } else if (typeof cardnumber !== "number") {
     throw new TypeError("Please insert only numbers!");
-  }
-  if (parseInt(cardnumber) && cardnumber.toString().length === 1) {
+
+  } else if (parseInt(cardnumber) && cardnumber.toString().length === 1) {
     throw new RangeError("The value insert has only one digit, please insert your complete card number!");
+
+  } else {
+    let numReverse = cardnumber.toString().split("").reverse().map(Number);
+    let numOperations = numReverse.map((num, i) => {
+      if (i % 2 === 1) {
+        return (num * 2 <= 9 ) ? num * 2 : num * 2 - 9;
+      } else {
+        return num;
+      }
+    });
+
+    let result = numOperations.reduce((acum, num) => acum + num);
+
+    return (result % 10 === 0) ? true : false;
   }
-
-  let numReverse = cardnumber.toString().split("").reverse().map(Number);
-  let numOperations = numReverse.map((num, i) => {
-    if (i % 2 === 1) {
-      return (num * 2 <= 9 ) ? num * 2 : num * 2 - 9;
-    } else {
-      return num;
-    }
-  });
-
-  let result = numOperations.reduce((acum, num) => acum + num);
-
-  return (result % 10 === 0) ? true : false;
 };
